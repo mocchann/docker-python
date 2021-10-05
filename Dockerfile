2e1597eed69beeea4f86c533306eb050b90bb69f
もっちゃん <s7056i3056@gmail.com>
2021-10-05 23:11:44 +0900

    beautifulsoupの導入

--- Dockerfile
+++ Dockerfile
@@ -16,3 +16,4 @@ RUN pip install --upgrade setuptools
 
 RUN python -m pip install jupyterlab
 RUN python -m pip install requests
+RUN python -m pip install beautifulsoup4
--- opt/sample.py
+++ opt/sample.py
@@ -1,9 +1,27 @@
 import math
 import sys
+import requests
+from bs4 import BeautifulSoup 
 
-def main():
-  val = float(sys.argv[1])
-  print(math.radians(val))
+res = requests.get('https://www.nap-camp.com/shimane/list?sortId=21&pageId=1')
 
-if __name__ == "__main__":
-  main()
+soup = BeautifulSoup(res.text, 'html.parser')
+
+title_text = soup.find('title').get_text()
+print(title_text)
+
+links = [url.get('href') for url in soup.find_all('a')]
+print(links)
+
+quote_elms = soup.find_all('div', {'class': 'quote'})
+print(len(quote_elms))
+
+# CSS セレクターを使って author を全て取得する
+author_names = [n.get_text() for n in soup.select('div.quote small.author')]
+print(author_names)
+
+def get_piyo_text():
+  piyo = soup.select_one('div.hoge div.fuga div.piyo')
+  if not piyo:
+      return None
+  return piyo.get_text()
